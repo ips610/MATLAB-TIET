c7cf5febd7fafc1f8769c4d6dbcae9e52e4f2084
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-08-23 08:40:02 +0530

    code completed 4th lab

--- Fourth_Lab.m
+++ Fourth_Lab.m
@@ -26,7 +26,30 @@ generally
 
 %}
 
-
+syms x;
 clear all;
 clc;
 
+f = input("Enter function f: ");
+x0 = input("Enter initial guess: ");
+df = inline(diff(f(x)));
+tolerance = ("Enter tolerance: ");
+max = input("Enter Maximum no of iterations: ");
+count = 1;
+
+if df(x0) ~= 0
+    while count <= max
+
+        x1 = x0 - f(x0)/df(x0);
+        e = abs(x1 - x0);
+
+        if e < tolerance
+            break
+        end
+        x0 = x1;
+        count = count + 1;
+    end
+
+else
+    disp("Newton's Method Fails");
+end
