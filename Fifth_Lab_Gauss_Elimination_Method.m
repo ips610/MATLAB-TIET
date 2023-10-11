aa1b40c98885ee3881ce875e974c31c9d511fefa
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-10-11 08:30:36 +0530

    Gauss Elimination

--- Fifth_Lab_Gauss_Elimination_Method.m
+++ Fifth_Lab_Gauss_Elimination_Method.m
@@ -41,7 +41,7 @@ Gauss Elimination Method
 m21 = a21 / a11   --> R2 -> R2 - m21R1
 m31 = a31 / a11   --> R3 -> R3 - m31R1
 
-mi1 - ai1 / a11   --> Ri -> Ri - mi1R1  { i = 1, 2, ....., n }
+mi1 = ai1 / a11   --> Ri -> Ri - mi1R1  { i = 1, 2, ....., n }
 
 
 After this We will get upper triangular Matrix
@@ -80,12 +80,29 @@ x1 = ( b1' - u12 * x2 - u13 * x3 .... - u1n * xn ) / u11
 clear all;
 clc;
 
-A = [1 2 3;4 5 6;7 8 9];
-B = [10;11;12];
+% A = [1 2 3;4 5 6;7 8 9];
+% B = [10;11;12];
+% 
+% Augmented = [A B];
+% 
+% [rows,cols] = size(Augmented);
 
-Augmented = [A B];
-
-[rows,cols] = size(Augmented);
 
 
+A = [2 3 4 19; 4 11 14 55; 2 8 17 50];
+[n,m] = size(A);
+for i = 1: n - 1
+    for k = i + 1: n
+        multi = A(k,i) / A(i,i);
+        A(k, :) = -multi * A(i, :) + A(k, :);
+    end
+end
 
+A
+
+x = zeros(1,n);
+for i = n: -1: 1
+    x(i) = ((A(i,m) - sum(A(i,1:n) .* x)) / (A(i,i)));
+end
+
+x
