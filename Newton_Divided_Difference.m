23ff789ea78bddf45b97bc1dff9a0ee9fe05f99f
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-11-22 01:29:54 +0530

    Newton Divided Difference

--- Newton_Divided_Difference.m
+++ Newton_Divided_Difference.m
@@ -1,25 +1,25 @@
-clc;
-clear all;
-x = [-1 0 1 2];
-y = [5 1 1 11];
-X = input("Input the value of X: ");
-[~,n] = size(x);
+clc
+clear all
+
+x = [0 1 2 4 5 6];
+y = [1 14 15 5 6 19];
+X = 3;
+n = size(x,2);
 DD = zeros(n,n);
 DD(:,1) = y';
-for j=2:n
-    for i=1:n-j+1
-        num = DD(i+1,j-1) - DD(i,j-1);
+
+for j = 2:n
+    for i=1:(n-j+1)
+        num = DD(i+1,j-1)-DD(i,j-1);
         den = (x(i+j-1)-x(i));
         DD(i,j) = num./den;
-    end
+    end 
 end
 
-for i=1:size(x,2)-1 %size(x,2) means column      size(x,1) means row
-    prodx = prod(X-x(:,1:i))*DD(1,i+1); 
-    % X - x(1:i,:)
-    P(i) = prodx;
+for i=1:size(x,2)-1
+    P(i) = prod(X-x(:,1:i))*DD(1,i+1);
 end
 
-Y = y(1)+sum(P);
+y = y(1) + sum(P);
+
 array2table(DD)
-fprintf('Value of Y at X=%f is %f\n',X,Y);
