fb65808f2300c9bfd1dec9232eadd1e7a50a35ca
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-11-21 16:43:00 +0530

    Power Method

---
+++ Power_Mehtod.m
@@ -0,0 +1,20 @@
+clc
+clear all
+A=[2 -1 0;-1 2 -1;0 -1 2];
+x=[1;1;1];
+count = 1;
+tolerance=10^-4;
+error=1000;
+ans = Inf;
+while (error > tolerance)
+    xold = x;
+    y=A*x;
+    eigen_value = max(abs(y));
+    eigen_vector = y./eigen_value;
+    x=eigen_vector;
+    error = abs(sum(x-xold));
+    ans=eigen_value;
+end
+
+eigen_value
+eigen_vector
